d5847de8558d599cbed5428991ff4fefa4dceec9
Panindra <pani.1990@gmail.com>
2015-04-20 09:08:55 -0500

    close to final version

--- categoryClassification.py
+++ categoryClassification.py
@@ -55,50 +55,52 @@ graphics_counter = 0
 
 total_counter = 0
 
-k = 1
+k = 100
 v = 8
 
 def compute_probabilities():
+     global v
      for word in space_dict:
         val = space_dict.get(word)
-        p_space_dict.update({word: (math.log( ((val + k) * 1.0)/(total_space_counter + (k * v)) )) })
+        p_space_dict.update({word: (math.log( ((val + k) * 1.0)/(total_space_counter + k * v) )) })
 
      for word in hardware_dict:
         val = hardware_dict.get(word)
-        p_hardware_dict.update({word: (math.log( ((val + k) * 1.0)/(total_hardware_counter + (k * v)) )) })
+        p_hardware_dict.update({word: (math.log( ((val + k) * 1.0)/(total_hardware_counter + k * v) )) })
 
      for word in baseball_dict:
         val = baseball_dict.get(word)
-        p_baseball_dict.update({word: (math.log( ((val + k) * 1.0)/(total_baseball_counter + (k * v))) ) })
+        p_baseball_dict.update({word: (math.log( ((val + k) * 1.0)/(total_baseball_counter + k * v)) ) })
 
      for word in windows_dict:
         val = windows_dict.get(word)
-        p_windows_dict.update({word: (math.log( ((val + k) * 1.0)/(total_windows_counter + (k * v))) ) })
+        p_windows_dict.update({word: (math.log( ((val + k) * 1.0)/(total_windows_counter + k * v)) ) })
 
      for word in politics_dict:
         val = politics_dict.get(word)
-        p_politics_dict.update({word: (math.log( ((val + k) * 1.0)/(total_politics_counter + (k * v))) ) })
+        p_politics_dict.update({word: (math.log( ((val + k) * 1.0)/(total_politics_counter + k * v)) ) })
 
      for word in forsale_dict:
         val = forsale_dict.get(word)
-        p_forsale_dict.update({word: (math.log( ((val + k) * 1.0)/(total_forsale_counter + (k * v))) ) })
+        p_forsale_dict.update({word: (math.log( ((val + k) * 1.0)/(total_forsale_counter + k * v)) ) })
 
      for word in hockey_dict:
         val = hockey_dict.get(word)
-        p_hockey_dict.update({word: (math.log( ((val + k) * 1.0)/(total_hockey_counter + (k * v))) ) })
+        p_hockey_dict.update({word: (math.log( ((val + k) * 1.0)/(total_hockey_counter + k * v)) ) })
 
      for word in graphics_dict:
         val = graphics_dict.get(word)
-        p_graphics_dict.update({word: (math.log( ((val + k) * 1.0)/(total_graphics_counter + (k * v)) )) })
+        p_graphics_dict.update({word: (math.log( ((val + k) * 1.0)/(total_graphics_counter + k * v) )) })
 
-     p_space = math.log((total_space_counter *1.0)/ total_counter)
-     p_hardware = math.log((total_hardware_counter *1.0)/ total_counter)
-     p_baseball = math.log((total_baseball_counter *1.0)/ total_counter)
-     p_windows = math.log((total_windows_counter *1.0)/ total_counter)
-     p_politics = math.log((total_politics_counter *1.0)/ total_counter)
-     p_forsale = math.log((total_forsale_counter *1.0)/ total_counter)
-     p_hockey = math.log((total_hockey_counter *1.0)/ total_counter)
-     p_graphics = math.log((total_graphics_counter *1.0)/ total_counter)
+     global p_space, p_hardware, p_baseball, p_windows, p_politics, p_forsale, p_hockey, p_graphics
+     p_space = math.log((space_counter *1.0)/ total_counter)
+     p_hardware = math.log((hardware_counter *1.0)/ total_counter)
+     p_baseball = math.log((baseball_counter *1.0)/ total_counter)
+     p_windows = math.log((windows_counter *1.0)/ total_counter)
+     p_politics = math.log((politics_counter *1.0)/ total_counter)
+     p_forsale = math.log((forsale_counter *1.0)/ total_counter)
+     p_hockey = math.log((hockey_counter *1.0)/ total_counter)
+     p_graphics = math.log((graphics_counter *1.0)/ total_counter)
 
 def compute_odds_ratio(class1, class2):
     odds_ratio_dict = {}
@@ -119,6 +121,7 @@ def compute_odds_ratio(class1, class2):
 def calc_accuracy():
     correct_class = 0
     total_rows = 0
+    Matrix = [[0 for x in range(8)] for x in range(8)]
 
     with open(HomeDirectory+ "8category.testing.txt")as f:
         test_data = f.readlines()
@@ -139,30 +142,31 @@ def calc_accuracy():
           for word in words:
               split_word = word.split(":")
               keyword = split_word[0]
+              frequency = int(split_word[1]) * 1.0
 
               if(p_space_dict.has_key(keyword)):
-                  val_space += p_space_dict.get(keyword)
+                  val_space += p_space_dict.get(keyword) * frequency
 
               if(p_hardware_dict.has_key(keyword)):
-                  val_hardware += p_hardware_dict.get(keyword)
+                  val_hardware += p_hardware_dict.get(keyword) * frequency
 
               if(p_baseball_dict.has_key(keyword)):
-                  val_baseball += p_baseball_dict.get(keyword)
+                  val_baseball += p_baseball_dict.get(keyword) * frequency
 
               if(p_windows_dict.has_key(keyword)):
-                  val_windows += p_windows_dict.get(keyword)
+                  val_windows += p_windows_dict.get(keyword) * frequency
 
               if(p_politics_dict.has_key(keyword)):
-                  val_politics += p_politics_dict.get(keyword)
+                  val_politics += p_politics_dict.get(keyword) * frequency
 
               if(p_forsale_dict.has_key(keyword)):
-                  val_forsale += p_forsale_dict.get(keyword)
+                  val_forsale += p_forsale_dict.get(keyword) * frequency
 
               if(p_hockey_dict.has_key(keyword)):
-                  val_hockey += p_hockey_dict.get(keyword)
+                  val_hockey += p_hockey_dict.get(keyword) * frequency
 
               if(p_graphics_dict.has_key(keyword)):
-                  val_graphics += p_graphics_dict.get(keyword)
+                  val_graphics += p_graphics_dict.get(keyword) * frequency
 
 
           val_space += p_space
@@ -175,11 +179,16 @@ def calc_accuracy():
           val_graphics += p_graphics
 
           high_class_values = [val_space, val_hardware, val_baseball, val_windows, val_politics, val_forsale, val_hockey, val_graphics]
-          if(class_val == give_class_val(high_class_values)):
+          classifed_class = give_class_val(high_class_values)
+          if(class_val == classifed_class):
               correct_class += 1
+
+          Matrix[class_val][classifed_class] += 1
 
         print(correct_class)
         print((correct_class * 1.0)/ total_rows)
+        for line in Matrix:
+            print(line)
 
 def give_class_val(class_values):
     index = 0
@@ -280,6 +289,22 @@ if __name__ == "__main__":
       for line in training_data:
           total_counter += 1
           class_val = int(line[0])
+          if(class_val == 0):
+              space_counter += 1
+          elif(class_val == 1):
+              hardware_counter += 1
+          elif(class_val == 2):
+              baseball_counter += 1
+          elif(class_val == 3):
+              windows_counter += 1
+          elif(class_val == 4):
+              politics_counter += 1
+          elif(class_val == 5):
+              forsale_counter += 1
+          elif(class_val == 6):
+              hockey_counter += 1
+          elif(class_val == 7):
+              graphics_counter += 1
 
           words = line[2:].split(" ")
           for word in words:
@@ -289,7 +314,6 @@ if __name__ == "__main__":
               val = 0
 
               if(class_val == 0):
-                  space_counter += 1
                   total_space_counter += frequency
                   if space_dict.has_key(current_word):
                     val = space_dict.get(current_word)
@@ -299,7 +323,6 @@ if __name__ == "__main__":
                     space_dict.update({current_word: frequency})
 
               elif(class_val == 1):
-                  hardware_counter += 1
                   total_hardware_counter += frequency
                   if hardware_dict.has_key(current_word):
                     val = hardware_dict.get(current_word)
@@ -309,7 +332,6 @@ if __name__ == "__main__":
                     hardware_dict.update({current_word: frequency})
 
               elif(class_val == 2):
-                  baseball_counter += 1
                   total_baseball_counter += frequency
                   if baseball_dict.has_key(current_word):
                     val = baseball_dict.get(current_word)
@@ -319,7 +341,6 @@ if __name__ == "__main__":
                     baseball_dict.update({current_word: frequency})
 
               elif(class_val == 3):
-                  windows_counter += 1
                   total_windows_counter += frequency
                   if windows_dict.has_key(current_word):
                     val = windows_dict.get(current_word)
@@ -329,7 +350,6 @@ if __name__ == "__main__":
                     windows_dict.update({current_word: frequency})
 
               elif(class_val == 4):
-                  politics_counter += 1
                   total_politics_counter += frequency
                   if politics_dict.has_key(current_word):
                     val = politics_dict.get(current_word)
@@ -339,7 +359,6 @@ if __name__ == "__main__":
                     politics_dict.update({current_word: frequency})
 
               elif(class_val == 5):
-                  forsale_counter += 1
                   total_forsale_counter += frequency
                   if forsale_dict.has_key(current_word):
                     val = forsale_dict.get(current_word)
@@ -349,7 +368,6 @@ if __name__ == "__main__":
                     forsale_dict.update({current_word: frequency})
 
               elif(class_val == 6):
-                  hockey_counter += 1
                   total_hockey_counter += frequency
                   if hockey_dict.has_key(current_word):
                     val = hockey_dict.get(current_word)
@@ -359,7 +377,6 @@ if __name__ == "__main__":
                     hockey_dict.update({current_word: frequency})
 
               elif(class_val == 7):
-                  graphics_counter += 1
                   total_graphics_counter += frequency
                   if graphics_dict.has_key(current_word):
                     val = graphics_dict.get(current_word)
@@ -368,8 +385,21 @@ if __name__ == "__main__":
                   else:
                     graphics_dict.update({current_word: frequency})
 
+
+    space_keys = set(space_dict.keys())
+    hardware_keys = set(hardware_dict.keys())
+    baseball_keys = set(baseball_dict.keys())
+    windows_keys = set(windows_dict.keys())
+    politics_keys = set(politics_dict.keys())
+    forsale_keys = set(forsale_dict.keys())
+    hockey_keys = set(hockey_dict.keys())
+    graphics_keys = set(graphics_dict.keys())
+
+    s = set.intersection(space_keys, hardware_keys, baseball_keys, windows_keys, politics_keys, forsale_keys, hockey_keys, graphics_keys)
+    v = len(space_keys) + len(hardware_keys) + len(baseball_keys) + len(windows_keys) + len(politics_keys) + len(forsale_keys) + len(hockey_keys) + len(graphics_keys) - len(s)
+
     main()
-    compute_odds_ratio(p_space_dict, p_hardware_dict)
-    compute_odds_ratio(p_graphics_dict, p_baseball_dict)
-    compute_odds_ratio(p_hockey_dict, p_baseball_dict)
-    compute_odds_ratio(p_forsale_dict, p_politics_dict)
+    compute_odds_ratio(p_space_dict, p_politics_dict)
+    compute_odds_ratio(p_windows_dict, p_graphics_dict)
+    compute_odds_ratio(p_hardware_dict, p_graphics_dict)
+    compute_odds_ratio(p_hardware_dict, p_windows_dict)
